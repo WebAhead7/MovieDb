1a06b860fa03e66df043a84cc10fc488da4798a5
nuwrss <nuwrssazzam@gmail.com>
2020-10-22 11:50:43 +0300

    bug fixes

--- src/home.js
+++ src/home.js
@@ -1,26 +1,37 @@
 var slideIndex = 1;
+var slideIndexNowPlaying = 1;
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n,type) {
+  
+  showSlides(n,type);
+ 
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
+
+function showSlides(n,type) {
+  var slides;
+  var indexOfSlides ;
+  if(type=='toprated'){
+    slideIndex+=n;
+    indexOfSlides = slideIndex;
+     slides = document.getElementsByClassName("mySlides");
+  }else{
+    slideIndexNowPlaying+=n;
+    indexOfSlides = slideIndexNowPlaying;
+     slides = document.getElementsByClassName("mySlides2");
+  }
     var i;
-    const slides = document.getElementsByClassName("mySlides");
+   
     
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) {indexOfSlides = 1}
+    if (n < 1) {indexOfSlides = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
    
-    console.log( slides.length);
-    slides[slideIndex-1].style.display = "block";
+   
+    slides[indexOfSlides-1].style.display = "block";
    
 }
 
@@ -71,9 +82,65 @@ data.results.forEach(element => {
    })
 
 });
-showSlides(slideIndex);
+showSlides(slideIndex,"toprated");
 })
-.catch((err) => console.log(err));
+.catch((err) => {
+  alert("Problem when loading the page please try again later");
+  console.log(err)}
+  );
+
+
+
+
+  const slideNP = document.getElementById('slideNowPlaying');
+
+  fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=aa48f2eff7284b7fe6bb91871f64f80d&language=en-US&page=1")
+
+.then(Response => {
+    console.log(Response);
+    if(!Response.ok){
+     throw new console.error(errrooooor);
+    }
+    return Response.json();
+})
+.then(function(data){
+    
+console.log(data);
+data.results.forEach(element => {
+   
+    var divcon2 = document.createElement('div');
+    
+   
+    var Img2 = document.createElement('img');
+    Img2.style.width="550px";
+    Img2.style.height="500px"
+    Img2.style.cursor="pointer";
+    Img2.style.marginLeft="50%";
+    Img2.style.transform="translateX(-50%)";
+    Img2.style.verticalAlign='middle';
+    slideNP.appendChild(divcon2);
+    divcon2.classList.add("mySlides2")
+    //divcon.appendChild(divNumtext);
+    divcon2.appendChild(Img2);
+    //divcon.appendChild(divText);
+    console.log('here');
+
+
+   Img2.src = 'http://image.tmdb.org/t/p/w185'+ element.poster_path;
+
+   divcon2.addEventListener('click',function(){
+    window.open("src/themovie.html?id="+element.id);
+   })
+
+});
+
+showSlides(slideIndexNowPlaying,"nowplaying");
+})
+.catch((err) => {
+  alert("Problem when loading the page please try again later");
+  console.log(err)}
+  );
+
 
 //fetch URL now playing
 // fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=aa48f2eff7284b7fe6bb91871f64f80d&language&language=en-US&page=1")
